d53eb09657cfbff574b484d86e40aa740bd408a4
José Ronaldo <dev.joseronaldo@gmail.com>
2022-06-26 16:42:07 -0300

    :sparkles: feat: Componente Header completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
+import { Event } from "./pages/event"
 
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-`
-interface Lessons{
-  id: string;
-  title:string;
-}
 
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY)
 
   return (
-   <ul>
-     {data?.lessons.map((lessons:Lessons) => {
-       return <li key={lessons.id}>{lessons.title}</li>
-     })}
-   </ul>
+    <div>
+     <Event/>
+    </div>
   )
 }
 
